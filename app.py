7ef33e46bdbbb1f7cc24fde2be934bb61e74c237
lokenath007 <lokenathghosh@gmail.com>
2024-04-05 17:03:59 +0530

    add files

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request,render_template
+
+app = Flask(__name__)
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
