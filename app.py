a56b263a10321ade254d1f36807a4e9a636529a8
lokenath007 <lokenathghosh@gmail.com>
2024-04-25 13:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request,render_template
 
 app = Flask(__name__)
 incomes = [
-    { 'description': 'salary', 'amount': 50000 }
+    { 'description': 'salary', 'amount': 500000 }
 ]
 
 
